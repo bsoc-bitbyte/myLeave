85f6ecaa0699db8518a8585bbb19cd8e58b83a89
AyushNarsian <ayushnarsian@yahoo.com>
2024-06-12 01:49:49 +0530

    Leave Form Page(fixed)

--- app/src/main/java/com/example/myleave/ui/user/LeaveFormActivity.kt
+++ app/src/main/java/com/example/myleave/ui/user/LeaveFormActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myleave.ui.user
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myleave.R
 
@@ -8,5 +10,25 @@ class LeaveFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leave_form)
+
+        val spinnerLeaveType: Spinner = findViewById(R.id.spinnerLeaveType)
+        val leaveTypeAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.leave_types,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        spinnerLeaveType.adapter = leaveTypeAdapter
+
+        val spinnerDepartment: Spinner = findViewById(R.id.spinnerDepartment)
+        val departmentAdapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.department_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        spinnerDepartment.adapter = departmentAdapter
     }
 }
