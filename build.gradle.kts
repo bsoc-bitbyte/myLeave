5a0f71f7109e23ba4fee3cebdd5d39c47c594f77
sujalagrawal2 <sujalagrawal2703@gmail.com>
2024-06-15 11:36:49 +0530

    Made the XML layout of leave request

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
 
