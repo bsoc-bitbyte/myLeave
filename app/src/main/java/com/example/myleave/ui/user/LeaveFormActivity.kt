fe49fe42557dc08bb4d289a10b55683c302d3c5c
sujalagrawal2 <sujalagrawal2703@gmail.com>
2024-06-29 17:26:54 +0530

    Implemented ImagePicker in LeaveFormActivity

--- app/src/main/java/com/example/myleave/ui/user/LeaveFormActivity.kt
+++ app/src/main/java/com/example/myleave/ui/user/LeaveFormActivity.kt
@@ -1,9 +1,19 @@
 package com.example.myleave.ui.user
 
+import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
+import android.provider.Settings
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myleave.R
 import com.example.myleave.databinding.ActivityLeaveFormBinding
 import java.text.SimpleDateFormat
@@ -15,6 +25,10 @@ class LeaveFormActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLeaveFormBinding
     private lateinit var startDate: Date
     private lateinit var endDate: Date
+    private val PICK_FILE_REQUEST_CODE = 1
+    private val REQUEST_PERMISSION_CODE = 2
+    private val requiredPermission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +68,115 @@ class LeaveFormActivity : AppCompatActivity() {
             openDatePicker(1)
         }
 
+        binding.ibChooseFile.setOnClickListener() {
+
+            if (isPermissionGranted()) {
+                openFilePicker()
+            } else {
+                requestPermission()
+            }
+
+
+        }
+
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            requiredPermission,
+            REQUEST_PERMISSION_CODE
+        )
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_CODE
+            && grantResults.isNotEmpty()
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            openFilePicker()
+        } else {
+            showSettingDialog()
+        }
+    }
+
+    private fun showSettingDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Permission Required")
+            .setMessage("This app needs access to your storage to upload files.")
+            .setPositiveButton("Go to Settings") { dialog, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", packageName, null)
+                }
+                startActivity(intent)
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun openFilePicker() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "*/*"
+            putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("application/pdf", "image/*"))
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+        }
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+
+    }
+
+    @Deprecated("This method has been deprecated in favor of using the Activity Result API")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_FILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                handleFileUri(uri)
+            }
+        }
+    }
+
+    private fun handleFileUri(uri: Uri) {
+        val fileName = getFileName(uri)
+        binding.etAttachment.setText(fileName)
+    }
+
+    private fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            cursor?.use {
+                if (it.moveToFirst()) {
+                    result = it.getString(it.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME))
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result?.lastIndexOf('/')
+            if (cut != -1) {
+                result = result?.substring(cut!! + 1)
+            }
+        }
+        if (result == "" || result == null) {
+            result = "Unknown file"
+        }
+        return result
     }
 
     private fun openDatePicker(check: Int) {
